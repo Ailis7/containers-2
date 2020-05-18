13034247a682a66735296ef8c404ac06f7563bc8
Petr Brovkin <bropet@mail.ru>
2020-05-18 22:46:28 +0300

    final

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+// TODO: write your code here
+import Character from './classes/Character';
+import Bowman from './classes/Bowman';
+import Swordsman from './classes/Swordsman';
+import Magician from './classes/Magician';
+import Undead from './classes/Undead';
+import Zombie from './classes/Zombie';
+import Daemon from './classes/Daemon';
+import Team from './classes/Team';
+import ErrorRepository from './classes/ErrorRepository';
